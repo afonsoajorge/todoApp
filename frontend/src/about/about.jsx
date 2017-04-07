1972ff815cc3c9ca8c2896dc5f04016fbc4872c5
Afonsoajorge <afonsoajorge@hotmail.com>
2017-04-07 12:07:25 -0300

    Projeto

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+        <h2>Nosso Historia</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto aut deleniti dicta dolorum
+            earum est et fugit illo illum incidunt iusto laboriosam modi recusandae rem sapiente sint, tenetur vero?</p>
+    </div>
+)
